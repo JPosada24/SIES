1c69d95ea39a61a5831c6e6edde66485efe9e06b
Julian Posada <jfposada63@misena.edu.co>
2021-03-18 10:50:19 -0500

    Creacion modelo usuario

---
+++ SIES/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIES.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public int Documento { get; set; }
+        public string TipoDoc { get; set; }
+        public string Nombre { get; set; }
+        public int Celular { get; set; }
+        public string Email { get; set; }
+        public string Genero { get; set; }
+        public string Aprendiz { get; set; }
+        public string Egresado { get; set; }
+        public string AreaFormacion { get; set; }
+        public DateTime FechaEgresado { get; set; }
+        public string Direccion { get; set; }
+        public string Barrio { get; set; }
+        public string Ciudad { get; set; }
+        public string Departamento { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}

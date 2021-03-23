fc39d4f20eb1988d90e15d5d7c6901e66da26aff
Julian Posada <64169045+JulianPosada16@users.noreply.github.com>
2021-03-23 08:59:43 -0500

    Se agrega el archivo de insertprueba de sql

---
+++ Querys Base de datos/InsertPrueba.sql
@@ -0,0 +1,19 @@
+
+INSERT INTO usuarios(
+usu_documento,
+usu_tipodoc,
+usu_nombre,
+usu_celular,
+usu_email,
+usu_genero,
+usu_aprendiz,
+usu_egresado,
+usu_areaformacion,
+usu_fechaegresado,
+usu_direccion,
+usu_barrio,
+usu_ciudad,
+usu_departamento,
+usu_fecharegistro)
+VALUES(
+34125532,'CC','Sebastián',312345234,'sebastian@gmail.com','M','SI','NO','ADSI','2018/03/23','Cra 17a n9-40','Verdum','Chinchiná','Caldas',GETDATE())
